f7ee7a59566ec9d464a9809f9b80ebad75d3daee
JoaquimCoelho <quimpedro1996@hotmail.com>
2024-02-26 15:17:52 +0000

    Mais APIs testadas e adicionadas

--- testes.py
+++ testes.py
@@ -1,6 +1,31 @@
+# GPT - OpenAI-----------------------------------------------------------------------------------------------
+"""
 import ast
 from openai import OpenAI
+import openai
 import pandas as pd
 import tiktoken
 import os
 from scipy import spatial
+
+# Carrega o modelo
+EMBEDDING_MODEL = "text-embedding-ada-002"
+GPT_MODEL = "gpt-3.5-turbo"
+
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+# an example question about the 2022 Olympics
+query = 'Which athletes won the gold medal in curling at the 2022 Winter Olympics?'
+
+response = client.chat.completions.create(
+    messages=[
+        {'role': 'system', 'content': 'You answer questions about the 2022 Winter Olympics.'},
+        {'role': 'user', 'content': query},
+    ],
+    model=GPT_MODEL,
+    temperature=0,
+)
+
+print(response.choices[0].message.content)
+------------------------------------------------------------------------------------------------------------
+"""
